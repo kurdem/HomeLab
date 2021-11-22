0ee7e69b671e0a7e2faed60a53f8d433c9cca3b7
kurdem <43165848+kurdem@users.noreply.github.com>
2021-11-22 20:38:31 +0100

    Update HomeLabOSD.ps1

--- HomeLabOSD.ps1
+++ HomeLabOSD.ps1
@@ -1,1 +1,15 @@
+Write-Host  -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1024
+}
+
 Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds! Eject USB or ISO"
+Start-Sleep -Seconds 20
+wpeutil reboot
